0db6b34b7e097e4faca4ceb6f7e8042acf1d26a8
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-06 15:09:38 +0200

    fix style in index and show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.base')
 
 @section('contents')
-<h1>Post</h1>
+<h1 class="text-center mt-3 mb-3">Post</h1>
 
 {{-- @if (session('delete_success'))
     <div class="alert alert-danger">
@@ -9,7 +9,7 @@
     </div>
 @endif --}}
 
-<table class="table">
+{{-- <table class="table">
     <thead>
         <tr>
             <th scope="col">ID</th>
@@ -24,7 +24,7 @@
             <th scope="row">{{ $post->id }}</th>
             <td>{{ $post->title }}</td>
             <td>{{ $post->url_image }}</td>
-            <td>
+            <td class="d-flex gap-2">
                 <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="btn btn-primary">View</a>
                 <a href="{{ route('admin.posts.edit', ['post' => $post->id]) }}" class="btn btn-warning">Edit</a>
                 <form
@@ -40,6 +40,28 @@
         @endforeach
     </tbody>
 </table>  
-{{ $posts->links() }}
+{{ $posts->links() }} --}}
+<div class="d-flex gap-2">
+@foreach ($posts as $post)
+    <div class="card" style="width: 18rem;">
+        <img src="{{ $post->url_image }}" class="card-img-top" alt="{{ $post->title }}">
+        <div class="card-body">
+            <h5 class="card-title">{{ $post->title }}</h5>
+            <p class="card-text">{{ $post->content }}</p>
+            <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="btn btn-primary align-self-end">View</a>
+            <a href="{{ route('admin.posts.edit', ['post' => $post->id]) }}" class="btn btn-warning">Edit</a>
+            <form
+            action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}"
+            method="POST"
+            class="d-inline-block">
+                @csrf
+                @method('delete')
+                <button class="btn btn-danger">Delete</button>
+            </form>
+        </div>
+    </div>
+    @endforeach
+</div>
+    {{ $posts->links() }}
 
 @endsection
