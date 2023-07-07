e5130d346e25e54c0e13d01f140119c179ae4d15
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-07 18:06:21 +0200

    add table in index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,79 @@
 @extends('admin.layouts.base')
 
 @section('contents')
-Sono l'index    
+<h1 class="text-center mt-3 mb-3">My Project</h1>
+
+{{-- @if (session('delete_success'))
+@php
+    $post = session('delete_success')
+@endphp
+    <div class="alert alert-danger">
+        Il post "{{ $post->title }}" è stato eliminato per sempre
+    </div>
+@endif --}}
+
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Link</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($projects as $project)
+        <tr>
+            <th scope="row">{{ $project->id }}</th>
+            <td>{{ $project->title }}</td>
+            <td>{{ $project->author}}</td>
+            <td><a href="{{ $project->url_github}}">{{ $project->url_github}}</a></td>
+            {{-- <td> --}}
+                {{-- <a href="{{ route('admin.projects.show', ['project' => $project]) }}" class="btn btn-primary">View</a> --}}
+                {{-- <a href="{{ route('admin.projects.edit', ['project' => $project]) }}" class="btn btn-warning">Edit</a> --}}
+                {{-- <button type="button" class="btn btn-danger js-delete" data-bs-toggle="modal" data-bs-target="#deleteModal" data-id="{{ $project->id }}"> --}}
+                    {{-- aggiungo data-id per passare data a js, inserirò qui l'identificatore...aggiugno codice js inn app.js --}}
+                    {{-- Delete --}}
+                {{-- </button> --}}
+                {{-- il button richiama il modal con data-bs-target --}}
+            {{-- </td> --}}
+        </tr>
+        @endforeach
+    </tbody>
+</table>  
+
+{{-- <div
+class="modal fade"
+id="deleteModal"
+data-bs-backdrop="static"
+data-bs-keyboard="false"
+tabindex="-1"
+aria-labelledby="deleteModalLabel"
+aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Conferma eliminazione!</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            Sei sicuro?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                <form
+                action="{{ route('admin.projects.destroy', ['project' => $project]) }}"
+                method="POST"
+                class="d-inline-block" 
+                id="confirm-delete">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-danger">Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div> --}}
+
+{{ $projects->links() }}
 @endsection
